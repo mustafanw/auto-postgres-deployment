b400f19dae14ef2cbea49b0c764fa83ea7656a52
Mustafa Neemuchwala <mustafa.neemuchwala@lntinfotech.com>
2020-05-27 22:25:49 +1000

    updated python file

--- execute_db_scripts.py
+++ execute_db_scripts.py
@@ -1,31 +1,24 @@
+import os
+import psycopg2
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 hostname = 'localhost'
 username = 'postgres'
 password = 'postgres'
 database = 'sprint30'
 
-
-import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-# Simple routine to run a query on a database and print the results:
 def doQuery( conn ) :
     cur = conn.cursor()
 
-    # cur.execute( "CREATE TABLE public.mustafa(action_trigger character varying)" )
-    print("inside do query 1")
     version_file = open('devdb.version','r')
     version = version_file.read()
-    print(version)
-    print(dir_path)
     sql_path = dir_path+'/sprint33/dev/postgres_dev_'+version+'.sql'
-    print(sql_path)
     sql_file = open(sql_path,'r')
     cur.execute(sql_file.read())
     conn.commit()
 
 
-
-import psycopg2
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
 doQuery( myConnection )
 myConnection.close()
